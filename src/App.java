a2b199f6a705169394e4dd515f583d79e25b9d8a
aloisiot <loistc.carvalho@gmail.com>
2021-09-10 14:04:01 -0300

    Correçao de logica no metodo main

--- src/App.java
+++ src/App.java
@@ -4,15 +4,18 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         String result = null;
-        
+        int b = 0;
+
         String num = JOptionPane.showInputDialog("Insira um número decimal (inteiro) para que seja convertido:");
         int n = Integer.parseInt(num);
         
-        String base = JOptionPane.showInputDialog("Insira a base (inetiro) para qual o número será convertido:");
-        int b = Integer.parseInt(base);
-
         while(result == null){
+            String base = JOptionPane.showInputDialog("Insira a base (inetiro entre 2 e 36) para qual o número será convertido:");
+            b = Integer.parseInt(base);
             result = BaseConverter.decimalToBaseX(n, b);
+
+            if(result == null)
+                JOptionPane.showMessageDialog(null, "Opção invalálida");
         }
         
         result = String.format("O número %d convertido para a base %d reulta em: %s", n, b, result);
