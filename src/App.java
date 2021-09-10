e4e68c42c7b032f1f629d9f59bd9a7e5ee28e400
aloisiot <loistc.carvalho@gmail.com>
2021-09-10 17:30:37 -0300

    Alteração de tipo int para long
    O uso do tipo primitivo int limita o uso do algoritmo, enquanto com o uso do tipo long o algoritmo abrange mais possibilidades

--- src/App.java
+++ src/App.java
@@ -1,24 +1,35 @@
-import javax.swing.JOptionPane;
+// import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String result = null;
-        int b = 0;
+        // String result = null;
+        // int b = 0;
 
-        String num = JOptionPane.showInputDialog("Insira um número decimal (inteiro) para que seja convertido:");
-        int n = Integer.parseInt(num);
+        // String num = JOptionPane.showInputDialog("Insira um número decimal (inteiro) para que seja convertido:");
+        // int n = Integer.parseInt(num);
         
-        while(result == null){
-            String base = JOptionPane.showInputDialog("Insira a base (inetiro entre 2 e 36) para qual o número será convertido:");
-            b = Integer.parseInt(base);
-            result = BaseConverter.decimalToBaseX(n, b);
+        // while(result == null){
+        //     String base = JOptionPane.showInputDialog("Insira a base (inetiro entre 2 e 36) para qual o número será convertido:");
+        //     b = Integer.parseInt(base);
+        //     result = BaseConverter.decimalToBaseX(n, b);
 
-            if(result == null)
-                JOptionPane.showMessageDialog(null, "Opção invalálida");
-        }
+        //     if(result == null)
+        //         JOptionPane.showMessageDialog(null, "Opção invalálida");
+        // }
         
-        result = String.format("O número %d convertido para a base %d reulta em: %s", n, b, result);
-        JOptionPane.showMessageDialog(null, result);                            
+        // result = String.format("O número %d convertido para a base %d reulta em: %s", n, b, result);
+        // JOptionPane.showMessageDialog(null, result);
+        String msg = "ALOISIO";
+        String encrip = BaseConverter.baseXToBaseY(msg, 30, 36);
+        
+        System.out.println("\nMensagem cripografada:");
+        System.out.println(encrip);
+        System.out.println("\n");
+        
+        String descript = BaseConverter.baseXToBaseY(encrip,36, 30);
+
+        System.out.println("Mensagem descriptografada:");
+        System.out.printf("%s%n", descript);
     }
 }
--- src/BaseConverter.java
+++ src/BaseConverter.java
@@ -4,37 +4,93 @@ import java.util.Stack;
  * Classe para conversão de bases numéricas
  */
 public class BaseConverter {
+
+    static final String DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     /**
-     * Converte um decima para qualquer base entre 2 e 36
-     * @param num Decimal a ser convertido
-     * @param base Base numérica para a qual o decimal será convertido
-     * @return String contendo o resultado da conversão na base desejada
+     * Converte um decima para seu equivalente em qualquer base entre 2 e 36.
+     * @param num Decimal a ser convertido.
+     * @param base Base numérica para a qual o decimal será convertido.
+     * @return String contendo o resultado da conversão na base desejada.
      */
-    public static String decimalToBaseX(int num, int base){
+    public static String decimalToBaseX(long number, int base){
 
-        double number = (double) num;
+        double num = (double) number;
         Stack<Integer> stack = new Stack<>();
         String result = "";
         double rem = 0;
-        String digits = "0123456789ABCDEFGHJKLMNOPQRSTUVWXYZ";
 
         if( base  < 2 || 36 < base){
             return null;
         }
 
-        while(number != 0){
+        while(num != 0){
 
-            rem = Math.floor( number % Double.valueOf(base));
-            number = Math.floor(number /base);
+            rem = Math.floor( num % Double.valueOf(base));
+            num = Math.floor(num /base);
             stack.push((int) rem);
 
         }
 
         while(!stack.isEmpty()){
-            result += digits.charAt((int) stack.pop());
+            result += DIGITS.charAt((int) stack.pop());
         }
 
+
+        return result;
+    }
+
+    /**
+     * Converte um número de qualquer base entre base 2 e base 36 para seu decima equivalente.
+     * @param number Número representado na base X a ser convertido para decimal.
+     * @param base Base em que se aapresenta o número a ser convertido.
+     * @return Decimal inteiro resultante da conversão.
+     */
+    public static long baseXToDecimal(String number, int base){
+
+        long result = 0;
+        String num = "";
+
+        if( base  < 2 || 36 < base){
+            return 0;
+        }
+
+        for(int i = number.length() - 1; i >= 0; i--){
+            num += number.charAt(i);
+        }
+
+
+        String.valueOf(number);
+        
+        for(int importace = 0; importace < num.length();importace++){
+            
+            for(int decimalValue = 0; decimalValue < DIGITS.length(); decimalValue++){
+            
+                if(num.charAt(importace) == DIGITS.charAt(decimalValue))
+                    result += decimalValue * Math.pow(base, importace);
+
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Converte um número representado em uma base X para seu equivalente em uma base Y 
+     * (As bases numéricas devem respeitar o intervalo entre 2 e 36).
+     * @param number Número inteiro representado em uma base X a ser convertido para o equivalente em uma base Y.
+     * @param baseX Base numérica do inteiro a ser convertido.
+     * @param baseY Base numérica para a qual o número será convertido.
+     * @return String contendo o  resultado da conversão.
+     */
+    public static String baseXToBaseY(String number, int baseX, int baseY){
+
+        long decimal = baseXToDecimal(number, baseX);
+        String result = decimalToBaseX(decimal, baseY);
+
+        if( baseX  < 2 || baseY < 2 || 36 < baseX || 36 < baseY){
+            return null;
+        }
 
         return result;
     }
