746a978b27693a675c8babf78df4762de3af1713
aloisiot <loistc.carvalho@gmail.com>
2021-09-10 13:16:02 -0300

    Primeiro algoritmo de conversao de bases finalizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String result = null;
+        
+        String num = JOptionPane.showInputDialog("Insira um número decimal (inteiro) para que seja convertido:");
+        int n = Integer.parseInt(num);
+        
+        String base = JOptionPane.showInputDialog("Insira a base (inetiro) para qual o número será convertido:");
+        int b = Integer.parseInt(base);
+
+        while(result == null){
+            result = BaseConverter.decimalToBaseX(n, b);
+        }
+        
+        result = String.format("O número %d convertido para a base %d reulta em: %s", n, b, result);
+        JOptionPane.showMessageDialog(null, result);                            
     }
 }
---
+++ src/BaseConverter.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+/**
+ * Classe para conversão de bases numéricas
+ */
+public class BaseConverter {
+    
+    /**
+     * Converte um decima para qualquer base entre 2 e 36
+     * @param num Decimal a ser convertido
+     * @param base Base numérica para a qual o decimal será convertido
+     * @return String contendo o resultado da conversão na base desejada
+     */
+    public static String decimalToBaseX(int num, int base){
+
+        double number = (double) num;
+        Stack stack = new Stack<Integer>();
+        String result = "";
+        double rem = 0;
+        String digits = "0123456789ABCDEFGHJKLMNOPQRSTUVWXYZ";
+
+        if( base  < 2 || 36 < base){
+            return null;
+        }
+
+        while(number != 0){
+
+            rem = Math.floor( number % Double.valueOf(base));
+            number = Math.floor(number /base);
+            stack.push((int) rem);
+
+        }
+
+        while(!stack.isEmpty()){
+            result += digits.charAt((int) stack.pop());
+        }
+
+
+        return result;
+    }
+}
